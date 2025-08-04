3d272639c6d4763cbff06d7e27a4aba84beceabc
tajreee <tajrianhaar@gmail.com>
2025-08-04 13:32:45 +0700

    feat: implement Company model with relationships and fillable attributes

---
+++ app/Models/Company.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Company extends Model
+{
+    protected $table = 'companies';
+    protected $primaryKey = 'user_id';
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'user_id',
+        'description',
+        'logo',
+    ];
+
+    /**
+     * Get the user that owns the Company
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+}
