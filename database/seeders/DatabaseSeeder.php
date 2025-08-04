cb174a5085c8254c317669ee1dbbc129002a5eaf
tajreee <tajrianhaar@gmail.com>
2025-08-04 22:23:31 +0700

    feat: add EmployeeSeeder to populate employees for the first 10 companies

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,7 +24,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             CompanySeeder::class,
-            // EmployeeSeeder::class,
+            EmployeeSeeder::class,
         ]);
     }
 }
